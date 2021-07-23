96d156d76db2c2f9a4252b7139556fc2adc13fe5
samuel <samuel.figs@gmail.com>
2021-07-23 17:09:21 -0300

    23-07 senai

--- POO_MVC/Controllers/LoginController.cs
+++ POO_MVC/Controllers/LoginController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using POO_MVC.Models;
@@ -7,26 +8,50 @@ namespace POO_MVC.Controllers
     public class LoginController : Controller
     {
         protected static string login = null;
+        protected static bool logado = false;
+
         public IActionResult Index()
-        {     
+        {
             ViewBag.Login = login;
+            ViewBag.Logado = logado;
             return View();
         }
+
+
 
         public IActionResult Logar(IFormCollection form)
         {
 
-            Participante acessoMetodos = new Participante();
+            Participante pMetodos = new Participante();
+            Musico mMetodos = new Musico();
 
-
-            if (acessoMetodos.Logar( ( form["Email"]).ToString(), (form["Senha"]).ToString() ) )
+            if ((form["Email"]).ToString().Contains("@") && (form["Email"]).ToString().Contains(".com"))
             {
-                login = $"Login realizado com sucesso. Bem vindo!";
+                if (pMetodos.Logar((form["Email"]).ToString(), (form["Senha"]).ToString()))
+                {
+                    logado = true;
+                    login = $"Login realizado com sucesso. Bem vindo!";
+                }
+                else
+                {
+                    logado = false;
+                    login = $"Usuario ou senha inválido. Tente novamente";
+                }
             }
             else
             {
-                login = $"Usuario ou senha inválido. Tente novamente";
+                if (mMetodos.Logar((form["Email"]).ToString(), (form["Senha"]).ToString()))
+                {
+                    logado = true;
+                    login = $"Login realizado com sucesso. Bem vindo!";
+                }
+                else
+                {
+                    logado = false;
+                    login = $"Usuario ou senha inválido. Tente novamente";
+                }
             }
+
 
             return LocalRedirect("~/Login");
         }
---
+++ POO_MVC/Controllers/RegistroEventoController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace POO_MVC.Controllers
+{
+    public class RegistroEventoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
